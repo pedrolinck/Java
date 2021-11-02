65221de04a2853573d09aa01bc31d545b0ec48ea
pedrolinck <pedro_linck@outlook.com>
2021-11-01 23:24:28 -0300

    Exercise about StringBuilder

---
+++ cap13/exeStringBuilder/Comment.java
@@ -0,0 +1,17 @@
+package cap13.exeStringBuilder;
+
+public class Comment {
+    private String text;
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
